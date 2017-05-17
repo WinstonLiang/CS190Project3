424045f3e6e8a6a60508f521aed5004463529da5
Raustana <raustana@gmail.com>
2017-05-17 13:52:01 -0700

    New Scripts, Prototype altered, basic prefabs have new code added.
    Let the Push Wars begin!

---
+++ CS190Project3/Assets/Scripts/GlobalTimer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalTimer : MonoBehaviour {
+
+    public float timer = 0;
+    public List<GameObject> moving;
+
+    bool GONG;
+
+	// Use this for initialization
+	void Start () {
+        timer = 0;
+        GONG = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+
+        if(timer >= 5 && !GONG)
+        {
+            // Signal for all to move Debug
+            Debug.Log("GONG");
+            GONG = true;
+
+            foreach(GameObject thing in moving)
+            {
+                thing.GetComponent<Movement>().SwitchMove();
+            }
+        }
+        if(timer >= 10)
+        {
+            // Signal that animations should have ended Debug
+            Debug.Log("UNGONG");
+            GONG = false;
+            timer = 0;
+        }
+	}
+}
--- CS190Project3/Assets/Scripts/RoomGen.cs
+++ CS190Project3/Assets/Scripts/RoomGen.cs
@@ -5,42 +5,72 @@ using UnityEngine;
 
 public class RoomGen : MonoBehaviour {
 
+    // This script generates the coordinates for either the PROTOTYPE map or the FINAL map.
+    // It is not designed for a "smart" read of a custom map; only the two for CS190 Project 3.
+
     public bool pre = true;
-    public Dictionary<string, bool> coordinates;
+    public Dictionary<string, ROOM> coordinates;
+
+    // This cannot be automagically added!
+    // Each ROOM object must be manually added per scene!
+    public List<ROOM> rooms;
 
     // Use this for initialization
     void Start () {
-        coordinates = new Dictionary<string, bool>();
-        // Creates a predeterminted, manufactured map
-        if(pre)
-        {
-            // Initialize all rooms as "enterable" (6x 5y map)
-            for(int i = 0; i < 6; i++)
-            {
-                for(int j = 0; j < 5; j++)
-                {
-                    var coord = i.ToString() + j.ToString();
-                    // Debug.Log(coord);
-                    coordinates.Add(coord, true);
-                }
-            }
-            // Remove the rooms based in Winston's notebook:
-            coordinates.Remove("00");
-            coordinates.Remove("02");
-            coordinates.Remove("03");
-            coordinates.Remove("12");
-            coordinates.Remove("32");
-            coordinates.Remove("40");
-            coordinates.Remove("52");
-            coordinates.Remove("53");
+        coordinates = new Dictionary<string, ROOM>();
 
-            // Debug function to see if correct rooms added then removed.
-            foreach (KeyValuePair<string, bool> entry in coordinates)
-            {
-                //Debug.Log(entry.ToString());
-            }
+        // Initialize all rooms (6x 5y map)
+        foreach (ROOM room in rooms)
+            coordinates.Add(room.coordinate, room);
+
+        //// Creates a predeterminted, manufactured map (FINAL MAP)
+        //if (pre)
+        //{
+
+        //    // Initialize all rooms as "enterable" (6x 5y map)
+        //    for (int i = 0; i < 6; i++)
+        //    {
+        //        for (int j = 0; j < 5; j++)
+        //        {
+        //            var coord = i.ToString() + j.ToString();
+        //            // Debug.Log(coord);
+        //            coordinates.Add(coord, true);
+        //        }
+        //    }
+        //    // Remove the rooms based in Winston's notebook:
+        //    coordinates["00"] = false;
+        //    coordinates["02"] = false;
+        //    coordinates["03"] = false;
+        //    coordinates["12"] = false;
+        //    coordinates["32"] = false;
+        //    coordinates["40"] = false;
+        //    coordinates["52"] = false;
+        //    coordinates["53"] = false;
+        //}
+
+        //// Creates the rough map for M3 (PROTOTYPE MAP)
+        //else
+        //{
+        //    // Initialize all rooms as "enterable" (3x 2y map)
+        //    for(int i = 0; i < 3; i++)
+        //    {
+        //        for(int j = 0; j < 2; j++)
+        //        {
+        //            var coord = i.ToString() + j.ToString();
+        //            coordinates.Add(coord, true);
+        //        }
+        //    }
+        //    // Remove the rooms based on the prototype map:
+        //    coordinates["01"] = false;
+        //    coordinates["21"] = false;
+        //}
+
+        // Debug function to see if correct rooms added then removed.
+        foreach (KeyValuePair<string, ROOM> entry in coordinates)
+        {
+            //Debug.Log(entry.ToString());
         }
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
