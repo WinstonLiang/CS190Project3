ad2571a16cbb18a1e799df92217dde4e86c40da4
Raustana <raustana@gmail.com>
2017-06-01 15:46:11 -0700

    Have some Monsters!

---
+++ CS190Project3/Assets/Scripts/Triggers/MONSTER_FARTHER.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MONSTER_FARTHER : AkTriggerBase
+{
+    public void Step()
+    {
+        if (triggerDelegate != null)
+        {
+            triggerDelegate(null);
+        }
+    }
+}
---
+++ CS190Project3/Assets/Scripts/Triggers/MONSTER_TOO_FAR.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MONSTER_TOO_FAR : AkTriggerBase
+{
+    public void Bye()
+    {
+        if (triggerDelegate != null)
+        {
+            triggerDelegate(null);
+        }
+    }
+}
